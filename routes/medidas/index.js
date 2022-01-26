602443c6db3bc6e5a5b6203b859552c819940c02
yesenia <ymartinezac@uoc.edu>
2022-01-26 19:28:13 -0400

    removed unused code

--- routes/medidas/index.js
+++ routes/medidas/index.js
@@ -13,28 +13,8 @@ const storage = multer.diskStorage({
   }
 });
 
-const upload = multer({ storage: storage })
-/*const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-      cb(null, DIR);
-  },
-  filename: (req, file, cb) => {
-      const fileName = file.originalname.toLowerCase().split(' ').join('-');
-      cb(null, uuidv4() + '-' + fileName)
-  }
-});
+const upload = multer({ storage: storage });
 
-var upload = multer({
-  storage: storage,
-  fileFilter: (req, file, cb) => {
-      if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg") {
-          cb(null, true);
-      } else {
-          cb(null, false);
-          return cb(new Error('Only .png, .jpg and .jpeg format allowed!'));
-      }
-  }
-});*/
 module.exports = (config) => {
 
   const medidaService = new MedidaService(config.mysql.client);
